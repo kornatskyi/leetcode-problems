49de170411fbb69a472c3666585f6cb04f350582
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-08-18 18:36:48 -0700

    removed nth node from end of list, implement with converting linkd list to array and back

--- data_structures/linkedList.js
+++ data_structures/linkedList.js
@@ -1,13 +1,32 @@
-class ListNode {
-    constructor(data) {
-        this.data = data
-        this.next = null
-    }
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
 }
 
 class LinkedList {
     constructor(head = null) {
-        this.head = head
+        this.head = head;
+    }
+
+    getFirst = () => {
+        return this.head;
+    }
+    getLast = () => {
+        let lastNode = this.head;
+        if (lastNode) {
+            while (lastNode.next) {
+                lastNode = lastNode.next
+            }
+        }
+        return lastNode
+    }
+
+    add = (node) => {
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            this.getLast().next = node;
+        }
     }
 
     size() {
@@ -19,24 +38,28 @@ class LinkedList {
         }
         return count;
     }
-
-    clear() {
-        this.head = null;
-    }
-
-    getLast() {
-        let lastNode = this.head;
-        if (lastNode) {
-            while (lastNode.next) {
-                lastNode = lastNode.next
-            }
-        }
-        return lastNode
-    }
-    getFirst() {
-        return this.head;
-    }
 }
 
+function arrayToLinkedList(array) {
 
+    const linkedList = new LinkedList()
+    for (let i = 0; i < array.length; i++) {
+        linkedList.add(new ListNode(array[i]))
 
+    }
+    return linkedList
+}
+
+function linkedListToArray(linkedList) {
+
+    const array = []
+    function recursion(node) {
+        if (node === null) {
+            return
+        }
+        array.push(node.val);
+        recursion(node.next);
+    }
+    recursion(linkedList.getFirst())
+    return array
+}
---
+++ not_finished/removeNthNodeFromEndOfList.js
@@ -0,0 +1,140 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+
+
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+
+    getFirst = () => {
+        return this.head;
+    }
+    getLast = () => {
+        let lastNode = this.head;
+        if (lastNode) {
+            while (lastNode.next) {
+                lastNode = lastNode.next
+            }
+        }
+        return lastNode
+    }
+
+    add = (node) => {
+        if (this.head === null) {
+            this.head = node;
+        } else {
+            this.getLast().next = node;
+        }
+    }
+
+    size() {
+        let count = 0;
+        let node = this.head;
+        while (node) {
+            count++;
+            node = node.next
+        }
+        return count;
+    }
+}
+
+function arrayToLinkedList(array) {
+
+    const linkedList = new LinkedList()
+    for (let i = 0; i < array.length; i++) {
+        linkedList.add(new ListNode(array[i]))
+
+    }
+    return linkedList
+}
+
+function linkedListToArray(linkedList) {
+
+    const array = []
+    function recursion(node) {
+        if (node === null) {
+            return
+        }
+        array.push(node.val);
+        recursion(node.next);
+    }
+    recursion(linkedList.getFirst())
+    return array
+}
+
+
+
+
+
+var removeNthFromEnd = function (head, n) {
+
+
+    const array = linkedListToArray(new LinkedList(head))
+    array.splice(array.length - n, 1)
+
+    
+    return arrayToLinkedList(array).getFirst()
+    
+};
+
+
+
+
+
+
+
+
+
+//tests
+test(removeNthFromEnd, { head: arrayToLinkedList([1, 2, 3, 4, 5]).getFirst(), n: 2 }, [1, 2, 3, 5]);
+test(removeNthFromEnd, { head: arrayToLinkedList([1]).getFirst(), n: 1 }, []);
+test(removeNthFromEnd, { head: arrayToLinkedList([1, 2]).getFirst(), n: 1 }, [1]);
+test(removeNthFromEnd, { head: arrayToLinkedList([2, 3, 1, 7]).getFirst(), n: 1 }, [2, 3, 1]);
+test(removeNthFromEnd, { head: arrayToLinkedList([1, 2, 3, 4]).getFirst(), n: 4 }, [2, 3, 4]);
+
+
+
+function test(func, input, output) {
+    console.log(linkedListToArray(new LinkedList(func(input.head, input.n))));
+
+    if (arraysEqual(linkedListToArray(new LinkedList(func(input.head, input.n))), output)) {
+        console.log("For ", input.head, " PASSED!!!");
+    } else {
+        console.log("For ", input, " FAILED!!!");
+    }
+
+}
+
+
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    // If you don't care about the order of the elements inside
+    // the array, you should sort both arrays here.
+    // Please note that calling sort on an array will modify that array.
+    // you might want to clone your array first.
+
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
